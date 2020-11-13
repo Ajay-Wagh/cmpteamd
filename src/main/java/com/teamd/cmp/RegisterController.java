a0f4b46a626b3d73db9e68318a956cc898cc03c8
aw454565 <aw454565@gmail.com>
2020-11-13 23:19:20 +0530

    Registration part working with mysql

--- src/main/java/com/teamd/cmp/RegisterController.java
+++ src/main/java/com/teamd/cmp/RegisterController.java
@@ -1,13 +1,35 @@
 package com.teamd.cmp;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterController {
-    @GetMapping("/register")
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    @PostMapping("/register")
     String register(@RequestParam("name") String name,@RequestParam("address") String address,@RequestParam("email") String email,@RequestParam("contact") String contact,@RequestParam("pass") String pass){
+        if(name.length()>0 && address.length()>0 && email.length()>0 && contact.length()>0 && pass.length()>0){
+            if(customerRepository.fetchCustomerByContact(contact).isEmpty())
+            {
+                Customer c=new Customer();
+                //c.setId(0);
+                c.setAddress(address);
+                c.setEmail(email);
+                c.setContact(contact);
+                c.setName(name);
+                c.setToken("NNNNNNNNN");
+                c.setPassword(pass);
+                customerRepository.save(c);
+                return "Your Registration ID is : "+ customerRepository.fetchCustomerByContact(contact).get(0).getId();
+            }
+        }
         return "Entered Data is "+name+" "+address+" "+email+" "+contact+" "+pass;
     }
 }
