5c9561a694575b66e7a3eae034d1515b8eaa6978
Ajay Wagh <47133621+Ajay-Wagh@users.noreply.github.com>
2020-11-13 17:49:13 +0530

    Add files via upload

---
+++ src/main/java/com/teamd/cmp/CmpApplication.java
@@ -0,0 +1,13 @@
+package com.teamd.cmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamd/cmp/CmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamd.cmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
