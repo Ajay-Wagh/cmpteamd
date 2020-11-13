5462dab7d9d14d6dfda556f3f7820410ee490135
aw454565 <aw454565@gmail.com>
2020-11-13 18:03:39 +0530

    Temp html added

--- src/main/java/com/teamd/cmp/CmpApplication.java
+++ src/main/java/com/teamd/cmp/CmpApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CmpApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CmpApplication.class, args);
 	}
-
 }
