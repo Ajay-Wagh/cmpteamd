b2a9056ae6ed42c8cd82db228474e5546eca148d
aw454565 <aw454565@gmail.com>
2020-11-14 11:17:33 +0530

    java ver change

--- src/main/java/com/teamd/cmp/RegisterController.java
+++ src/main/java/com/teamd/cmp/RegisterController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegisterController {
 
-
     @PostMapping("/register")
     String register(@RequestParam("name") String name,@RequestParam("address") String address,@RequestParam("email") String email,@RequestParam("contact") String contact,@RequestParam("pass") String pass){
         return "Entered Data is "+name+" "+address+" "+email+" "+contact+" "+pass;
