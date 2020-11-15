0497ebc64c0e2695643da8bca0bdc1cda6401a16
aw454565 <aw454565@gmail.com>
2020-11-15 13:29:27 +0530

    Jpa data access

---
+++ src/main/java/com/teamd/cmp/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.teamd.cmp;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer,Integer>{
+
+    @Query("SELECT a FROM Customer a WHERE a.contact=:contact")
+    List<Customer> fetchCustomers(@Param("contact") String contact);
+}
